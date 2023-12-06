4e07ce06df9acec4273457b04d109b0ad3ce548d
Evan Phoenix <evan@phx.io>
2023-12-05 20:11:31 -0800

    Add ability to profile binary

--- cmd/lsvd/main.go
+++ cmd/lsvd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"runtime/pprof"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/lab47/lsvd"
@@ -12,8 +15,9 @@ import (
 )
 
 var (
-	fPath = flag.String("path", "./data", "path to store objects in")
-	fAddr = flag.String("addr", ":8989", "address to listen on")
+	fPath    = flag.String("path", "./data", "path to store objects in")
+	fAddr    = flag.String("addr", ":8989", "address to listen on")
+	fProfile = flag.Bool("profile", false, "enable profiling")
 )
 
 func main() {
@@ -21,11 +25,27 @@ func main() {
 
 	log := hclog.New(&hclog.LoggerOptions{
 		Name:  "lsvd",
-		Level: hclog.Info,
+		Level: hclog.Trace,
 		Color: hclog.AutoColor,
 
 		ColorHeaderAndFields: true,
 	})
+
+	if *fProfile {
+		f, err := os.Create("lsvd.profile")
+		if err != nil {
+			log.Error("unable to enable profiling", "error", err)
+			os.Exit(1)
+		}
+
+		defer func() {
+			pprof.StopCPUProfile()
+			f.Close()
+			log.Info("wrote profiling information", "path", "lsvd.profile")
+		}()
+
+		pprof.StartCPUProfile(f)
+	}
 
 	path, err := filepath.Abs(*fPath)
 	if err != nil {
@@ -44,6 +64,15 @@ func main() {
 		log.Error("error listening on addr", "error", err, "addr", *fAddr)
 		os.Exit(1)
 	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	go func() {
+		<-ctx.Done()
+		log.Info("shutting down")
+		l.Close()
+	}()
 
 	exports := []*nbd.Export{
 		{
